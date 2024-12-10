f219c416ce34c11fefadd6d9cb6ee0c6017abe34
Apurba <apurba1907@gmail.com>
2024-12-10 20:18:24 +0530

    GET

--- src/test/java/com/apurba/in/ex01_sampleCheck/apitesting001.java
+++ src/test/java/com/apurba/in/ex01_sampleCheck/apitesting001.java
@@ -1,4 +1,7 @@
 package com.apurba.in.ex01_sampleCheck;
 
 public class apitesting001 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 }
---
+++ src/test/java/com/apurba/in/ex02_RestAssuredBasics/GET/apitesting006.java
@@ -0,0 +1,4 @@
+package com.apurba.in.ex02_RestAssuredBasics.GET;
+
+public class apitesting006 {
+}
